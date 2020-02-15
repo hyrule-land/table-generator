e4246f6dbb26d499eece25ddff1cc0e2ed6e520a
qinhui <932433511@qq.com>
2020-02-15 23:00:52 +0800

    init project

---
+++ src/pages/index.js
@@ -0,0 +1,134 @@
+import styles from './index.css';
+import { formatMessage } from 'umi-plugin-locale';
+
+
+export default function() {
+  return (
+    <div className={styles.normal}>
+      <table className="lake-table">
+        <tbody>
+        <tr>
+          <td><p>abbbb</p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+        </tr>
+        <tr>
+          <td><p><br /></p></td>
+          <td><p>多多发发</p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+        </tr>
+        <tr>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+        </tr>
+        <tr>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p>发发啊啊啊</p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+        </tr>
+        <tr>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+        </tr>
+        <tr>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p>顶顶顶顶顶顶顶</p></td>
+          <td><p><br /></p></td>
+        </tr>
+        <tr>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+        </tr>
+        <tr>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+        </tr>
+        <tr>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+        </tr>
+        <tr>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+          <td><p><br /></p></td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
